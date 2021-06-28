c8b535dc6c2eb5268ef7602f31eed79feaeb9b3d
robbie-veglahn <61095659+robbie-veglahn@users.noreply.github.com>
2021-06-28 08:41:49 -0700

    Convert all datetime fields in dfs from strings into valid datetime objects for analysis, shorten some variable names for readability (#4)
    * change retrieve_submission_ids_json function to work with all states except Michigan (for now, MI will be fixed on backend)
    
    * convert all dateframe fields in DataFrames into valid datetime objects for analysis, shorten some variable names for readability
    
    * add user-agent security fix for MI endpoint
    
    * updated valid MI endpoints for testing

--- fetch.py
+++ fetch.py
@@ -1,6 +1,6 @@
 # more imports than we need
-import geopandas as gpd
-import maup
+# import geopandas as gpd
+# import maup
 import pandas as pd
 import matplotlib.pyplot as plt
 import requests
@@ -9,6 +9,7 @@ import csv
 import io
 import pydantic
 from pydantic import BaseModel
+from datetime import datetime as dt
 from typing import Tuple
 
 class Submission(pydantic.BaseModel):
@@ -22,7 +23,7 @@ class Submission(pydantic.BaseModel):
     id: str
 
 def submissions(ids_url: str, plans_url: str, cois_url: str,
-                wr_url: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+               wr_url: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
     """
     Takes in endpoint for all districtr ids in a portal along with csv api  ...
     calls for plans, cois, and written submissions, and retrieves filled pd ...
@@ -34,26 +35,38 @@ def submissions(ids_url: str, plans_url: str, cois_url: str,
                                                     if sub.plan_type == "plan"]
     coi_submissions = [sub.districtr_plan for sub in submissions #filters cois
                                                     if sub.plan_type == "coi"]
-    plan_submissions_df = csv_read(plans_url) # gathers plan metadata in df
-    coi_submissions_df = csv_read(cois_url) # gathers coi metadata in df
-    written_submissions_df = csv_read(wr_url) # gathers written metadata in df
-    assert len(plan_submissions) == len(plan_submissions_df)
-    assert len(coi_submissions) == len(coi_submissions_df)
+    plans_df = csv_read(plans_url) # gathers plan metadata in df
+    cois_df = csv_read(cois_url) # gathers coi metadata in df
+    written_df = csv_read(wr_url) # gathers written metadata in df
+    assert len(plan_submissions) == len(plans_df)
+    assert len(coi_submissions) == len(cois_df)
     # parse for plan id and add in submission dfs
-    plan_submissions_df['plan_id'] = plan_submissions_df["link"].map(
+    plans_df['plan_id'] = plans_df["link"].map(
                                 lambda link: link.split("/")[-1].split("?")[0])
-    coi_submissions_df['plan_id'] = coi_submissions_df["link"].map(
+    cois_df['plan_id'] = cois_df["link"].map(
                                 lambda link: link.split("/")[-1].split("?")[0])
     # sort dfs by plan id to correctly join w/ json information
-    plan_submissions_df = plan_submissions_df.sort_values(
-                                                by=['plan_id'], ascending=True)
-    coi_submissions_df = coi_submissions_df.sort_values(
-                                                by=['plan_id'], ascending=True)
+    plans_df = plans_df.sort_values(by=['plan_id'], ascending=True)
+    cois_df = cois_df.sort_values(by=['plan_id'], ascending=True)
     # join in districtr json assignments into 'districtr_data column'
-    plan_submissions_df['districtr_data'] = plan_submissions
-    coi_submissions_df['districtr_data'] = coi_submissions
+    plans_df['districtr_data'] = plan_submissions
+    cois_df['districtr_data'] = coi_submissions
+    # make datetime fields parseable:
+    plans_df['datetime'] = plans_df['datetime'].map( lambda datetime: (
+        datetime.split("+")[0] + " +" + datetime.split("+")[1].split(" ")[0]))
+    cois_df['datetime'] = cois_df['datetime'].map( lambda datetime: (
+        datetime.split("+")[0] + " +" + datetime.split("+")[1].split(" ")[0]))
+    written_df['datetime'] = written_df['datetime'].map( lambda datetime: (
+        datetime.split("+")[0] + " +" + datetime.split("+")[1].split(" ")[0]))
+    # # convert datetime fields from str's to datetime objects in all dataframe
+    plans_df['datetime'] = plans_df['datetime'].map(lambda datetime: (
+                                dt.strptime(datetime, '%a %b %d %Y %X %Z %z')))
+    cois_df['datetime'] = cois_df['datetime'].map(lambda datetime: (
+                                dt.strptime(datetime, '%a %b %d %Y %X %Z %z')))
+    written_df['datetime'] = written_df['datetime'].map(lambda datetime: (
+                                dt.strptime(datetime, '%a %b %d %Y %X %Z %z')))
     # return relevant dataframes
-    return plan_submissions_df, coi_submissions_df, written_submissions_df
+    return plans_df, cois_df, written_df
 
 def plan_read(plan_id: int) -> dict: #(dict: json obj)#
     """
@@ -70,7 +83,9 @@ def retrieve_submission_ids_json(url: str) -> list: #list: list[Submission]
     retrieveSubmissionJson takes a url (an endpoint to a given state's...
     submission portal), returns a list of filled Submission objects
     """
-    r = requests.get(url)
+    # TODO: temp fix for the purposes of user-agent api call barrier
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36'}  
+    r = requests.get(url, headers=headers)
     subs_json = json.loads(r.text)
     submissions = []
     for ids in subs_json['ids']:
--- test_fetch.py
+++ test_fetch.py
@@ -1,17 +1,17 @@
 import fetch
 
 def test_retrieve_submission_json():
-    url = "https://qp2072772f.execute-api.us-east-2.amazonaws.com/dev/submissions/districtr-ids"
+    url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/beta/submissions/districtr-ids/michigan"
     submissions = fetch.retrieve_submission_ids_json(url)
     assert submissions != 0
 
 def test_fetch_submissions():
-    url = "https://qp2072772f.execute-api.us-east-2.amazonaws.com/dev/submissions/districtr-ids"
-    plans_url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/prod/submissions/csv?type=plan&length=10000"
-    cois_url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/prod/submissions/csv?type=coi&length=10000"
-    written_url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/prod/submissions/csv?type=written&length=10000"
-    plans_df, cois_df, written_df = fetch.submissions(url, plans_url, cois_url, written_url)
+    ids_url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/beta/submissions/districtr-ids/michigan"
+    csv_url = "https://o1siz7rw0c.execute-api.us-east-2.amazonaws.com/beta/submissions/csv/michigan"
+    plans_url   = csv_url + "?type=plan&length=10000"
+    cois_url    = csv_url + "?type=coi&length=10000"
+    written_url = csv_url + "?type=written&length=10000"
+    plans_df, cois_df, written_df = fetch.submissions(ids_url, plans_url, cois_url, written_url)
     assert len(plans_df) != 0
     assert len(cois_df) != 0
     assert len(written_df) != 0
-
